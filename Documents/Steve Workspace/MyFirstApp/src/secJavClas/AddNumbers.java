54d863d43a1aebd50bbb4b6e2452e803b9ce6a86
sms1270 <38513643+sms1270@users.noreply.github.com>
2018-04-23 21:24:18 -0500

    MyFirstApp
    added AddNumbers class

---
+++ Documents/Steve Workspace/MyFirstApp/src/secJavClas/AddNumbers.java
@@ -0,0 +1,18 @@
+package secJavClas;
+
+import java.util.Scanner;
+
+class AddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x, y, z;
+		System.out.println("Enter two integers to calculate their sum ");
+		Scanner in = new Scanner(System.in);
+		x = in.nextInt();
+		y = in.nextInt();
+		z = x + y;
+		System.out.println("Sum of entered integers = " + z);
+	}
+}
