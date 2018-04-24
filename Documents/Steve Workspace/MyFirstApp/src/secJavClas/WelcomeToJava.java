d395bd684b3612ff81b8613a33d13661ccba5449
sms1270 <38513643+sms1270@users.noreply.github.com>
2018-04-23 19:10:14 -0500

    MyFirstApp
    Hello World Program

---
+++ Documents/Steve Workspace/MyFirstApp/src/secJavClas/WelcomeToJava.java
@@ -0,0 +1,12 @@
+package secJavClas;
+
+public class WelcomeToJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Here Is What Is Being Printed");
+
+	}
+
+}
