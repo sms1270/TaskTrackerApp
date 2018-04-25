6a20ecd25429fd119de1adf58039c46d8b8ee573
sms1270 <38513643+sms1270@users.noreply.github.com>
2018-04-25 07:29:14 -0500

    finally got it pushed correctly.

---
+++ MyFirstApp/src/calculator/Calculator.java
@@ -0,0 +1,53 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a, b, total = 0;
+		System.out.println("Enter two integers to calculate their sum ");
+		Scanner in = new Scanner(System.in);
+		a = in.nextInt();
+		b = in.nextInt();
+		calculateDifference(a,b);
+		calculateProduct(a,b);
+		calculateDivision(a,b);
+		
+		
+	}
+	
+	public static int calculateDifference(int a_difference, int b_difference) {
+		int total_difference = b_difference - a_difference;
+		System.out.println("The difference is " + total_difference);
+		return 0;
+		}
+	
+	public static int calculateProduct(int a_product, int b_product) {
+		int total_product = b_product * a_product;
+		System.out.println("The product is " + total_product);
+		return 0;
+		}
+		
+	public static int calculateDivision(int a_division, int b_division) {
+		int total_division = b_division / a_division;
+		int total_remainder = b_division % a_division;
+		if ( total_remainder == 0) {
+			System.out.println("The dividend is " + total_division);
+		}
+		else {
+			System.out.println("The dividend is " + total_division);
+			System.out.println("The remainder is " + total_remainder);
+		}
+		
+		return 0;
+		}
+}
+
+
+
+
+
+
