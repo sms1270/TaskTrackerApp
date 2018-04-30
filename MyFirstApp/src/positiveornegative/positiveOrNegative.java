3184f8e3ff9d38902deb22c019c959f4a6f49499
sms1270 <38513643+sms1270@users.noreply.github.com>
2018-04-29 21:27:20 -0500

    adding an array list..

--- MyFirstApp/src/positiveornegative/positiveOrNegative.java
+++ MyFirstApp/src/positiveornegative/positiveOrNegative.java
@@ -1,70 +1,66 @@
 package positiveornegative;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class positiveOrNegative {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		int x, y, z;
-		System.out.println("Enter three integers to calculate their sum ");
-		Scanner in = new Scanner(System.in);
-		x = in.nextInt();
-		y = in.nextInt();
-		z = in.nextInt();
-		
-		pickADirection(x,y,z);
-		greatestNumber(x,y,z);
-		calculator.Calculator.calculateDifference(x, y, z);
-		calculator.Calculator.calculateDivision(x,y,z);
-		calculator.Calculator.calculateProduct(x,y,z);
-	}
 	
-	public static void pickADirection(int input1, int input2, int input3) {
-		int a;
-		for (a = 0; a < 3; a += 1) {
-			if (input1 < 0) {
-				System.out.println("The first number " + input1 + " is a negative number");
-			}
-			else {
-				System.out.println("The first number " + input1 + " is a positive number");
-			}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
 			
-			if (input2 < 0) {
-				System.out.println("The second number " + input2 + " is a negative number");
-			}
-			else {
-				System.out.println("The second number " + input2 + " is a positive number");
-			}
+			int x, y,z;
+			System.out.println("Enter three integers to calculate their sum ");
+			Scanner in = new Scanner(System.in);
+			x = in.nextInt();
+			y = in.nextInt();
+			z = in.nextInt();
 			
-			if (input3 < 0 ) {
-				System.out.println("The third number " + input3 + " is a negative number");
-			}
-			else {
-				System.out.println("The third number " + input3 + " is a positive number");
-				System.out.println();
-			}
-			return;
-		}
-		}
-		
-		public static void greatestNumber(int number1, int number2, int number3) {
-			int a;
-			int biggest_number = 0;
-			for (a = 0; a < 3; a+= 1) {
-				if ((number1 > number2) && (number1 > number3)) {
-					biggest_number = number1;
-				}
-				else if ((number2 > number1) && (number2 > number3)) {
-					biggest_number = number2;
-				}
-				else if ((number3 > number1) && (number3 > number2))
-					biggest_number = number3;
-			}
-			System.out.println("The greatest number out the three integers is " + biggest_number);
-			return;
+			int listOfNumbers[] = {x, y, z};
+			
+			PickADirection(listOfNumbers);
+			greatestNumber(x,y,z);
+			calculator.Calculator.calculateDifference(x,y,z);
+			calculator.Calculator.calculateDivision(x,y,z);
+			calculator.Calculator.calculateProduct(x,y,z);
+	}
 
+
+	
+	public static void PickADirection(int numberList1[]) {
+		
+		int a = 0;
+		for (a=0; a<3; a+=1 ) {
+			if (numberList1[a] < 0) {
+				System.out.println("The number " + numberList1[a] + " is a negative number.");
+			}
+			else {
+				System.out.println("The number " + numberList1[a] + " is a positive number.");
+			}
 		}
+		return;
+	}
+		
+	public static void greatestNumber(int input1, int input2, int input3) {
+		int a = 0;
+		int greatest_number = 0;
+		for (a=0; a<3; a += 1 ) {
+			if ( (input1 > input2) && (input1 > input3) ) {
+				greatest_number = input1;
+			}  
+			else if ( (input2 > input1) && (input2 > input3) ) {
+				greatest_number = input2;
+			}
+			else if ( (input3 > input1 ) && (input3 > input2) ) {
+				greatest_number = input3;
+			}
+		}
+			System.out.println("The greatest number is " + greatest_number);
+			return;
+			
+	}
 }
+	
+	
+	
+
 
